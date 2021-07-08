6923f301fb7c54600cca4ca4dd314a5dc31123aa
Kalju Lee <kaljuvlee@gmail.com>
2021-07-08 13:05:09 -0400

    react table

--- src/components/ScheduleBoard/index.js
+++ src/components/ScheduleBoard/index.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
 
 function ScheduleBoard(props) {
-  return <div />;
+  const data = useMemo(
+    () => [
+      {
+        col1: 'd1 c1',
+        col2: 'd1 c2'
+      },
+      { col1: 'd2 c1', col2: 'd2 c2' }
+    ],
+    []
+  );
+
+  const columns = useMemo(
+    () => [
+      { header: 'column 1', accessor: 'col1' },
+      { header: 'column 2', accessor: 'col2' }
+    ],
+    []
+  );
+
+  const tableInstance = useTable({ columns, data });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <table {...getTableProps()}>
+      <thead />
+    </table>
+  );
 }
 
 export default ScheduleBoard;
