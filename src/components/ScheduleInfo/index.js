2e57e018dd23ad21f46bcea7779e87a19ac5741d
Kalju Lee <kaljuvlee@gmail.com>
2021-07-09 11:48:35 -0400

    flip box

--- src/components/ScheduleInfo/index.js
+++ src/components/ScheduleInfo/index.js
@@ -4,13 +4,14 @@ import colors from '../../styles/colors';
 function Square(props) {
   const { value } = props;
   return (
-    <div style={{ height: 50, width: 30 }}>
+    <div style={{ height: 47, width: 30 }}>
       <div
         style={{
           backgroundColor: colors.flipBack,
           border: `2px solid ${'black'}`,
           textAlign: 'center',
-          top: 2
+          top: 2,
+          height: '100%'
         }}
       >
         <span
@@ -19,8 +20,7 @@ function Square(props) {
             textAlign: 'center',
             fontSize: 32,
             color: colors.boardLettering,
-            position: 'relative',
-            top: -2
+            position: 'relative'
           }}
         >
           {value}
@@ -32,14 +32,27 @@ function Square(props) {
           backgroundColor: colors.boardBack,
           height: 2,
           width: 30,
-          top: -24
+          top: -25
         }}
       />
     </div>
   );
 }
 
+function convertStringToFlip(message) {
+  const output = [];
+  for (let i = 0; i < message.length; i += 1) {
+    output.push(<Square value={message[i]} />);
+  }
+  return output;
+}
+
+function FlipRow(props) {
+  return <div style={{ display: 'flex', width: '100%' }}>{props.children}</div>;
+}
+
 function ScheduleInfo(props) {
+  const { message } = props;
   return (
     <div
       style={{
@@ -48,17 +61,17 @@ function ScheduleInfo(props) {
         border: '1px solid purple'
       }}
     >
-      <div style={{ border: '1px solid yellow' }}>
+      <FlipRow style={{ border: '1px solid yellow' }}>
         <Square value="X" />
-      </div>
-      <div>
-        <Square value="p" />
-      </div>
-      <div style={{ border: '1px solid orange' }}>
+      </FlipRow>
+      <FlipRow>{convertStringToFlip(message)}</FlipRow>
+      <FlipRow style={{ border: '1px solid orange' }}>
         <Square value="x" />
-      </div>
+      </FlipRow>
     </div>
   );
 }
+
+ScheduleInfo.defaultProps = { message: 'DEFAULT MESSAGE Default Message' };
 
 export default ScheduleInfo;
