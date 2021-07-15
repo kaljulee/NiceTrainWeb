877436f214c0a2fe8a6ddd3c5eda6e364b2290fe
Kalju Lee <kaljuvlee@gmail.com>
2021-07-15 13:09:10 -0400

    update data

--- src/data/index.js
+++ src/data/index.js
@@ -17,7 +17,8 @@ const data = {
       station: 'TURT',
       ground: 'Scissor Sweep',
       standing: 'Forward Rolls',
-      status: 'ON TIME'
+      status: 'ON TIME',
+      detailsLink: 'something goes here'
     },
     {
       date: '0712',
@@ -26,7 +27,8 @@ const data = {
       station: 'TURT',
       ground: 'Turtle variations',
       standing: 'Hip Toss',
-      status: 'UNSURE'
+      status: 'UNSURE',
+      detailsLink: 'something goes here'
     },
     {
       date: '0714',
@@ -35,6 +37,7 @@ const data = {
       station: 'TURT',
       ground: 'Scissor Sweep',
       standing: 'Hip Toss',
+      detailsLink: 'something goes here',
       status: 'ON TIME'
     }
   ]
