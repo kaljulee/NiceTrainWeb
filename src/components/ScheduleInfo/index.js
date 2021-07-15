0961e5b70ca923d0569b19932b6667ce23dc4f1e
Kalju Lee <kaljuvlee@gmail.com>
2021-07-15 16:35:58 -0400

    calculateCharacterCount function

--- src/components/ScheduleInfo/index.js
+++ src/components/ScheduleInfo/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCurrentWidth } from 'react-socks';
 import colors from '../../styles/colors';
 import { FlipRow } from '../FlipText/FlipRow';
 import flipConstants from '../FlipText/flipConstants';
 
+function calculateCharacterCount(width) {
+  return Math.floor(width / flipConstants.width) - 2;
+}
+
 function ScheduleInfo(props) {
   const { message } = props;
   const width = useCurrentWidth();
-  const characterCount = Math.floor(width / flipConstants.width) - 2;
-  // const emptyMessageArray = [];
-  //
-  // const emptyMessage =
+  const [characterCount, setCharacterCount] = useState(
+    calculateCharacterCount(width)
+  );
+  useEffect(() => setCharacterCount(calculateCharacterCount(width)), [width]);
   console.log(characterCount);
   return (
     <div
       style={{
         backgroundColor: colors.boardComponent,
         width: '100%',
-        // paddingTop: '1vh',
-        //   paddingBottom: '1v
         padding: '1vh 1vw 1vh 1vw'
       }}
     >
