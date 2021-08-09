62b99cb7cc51b1ebed5fc66cae08a8aa35534da7
Kalju Lee <kaljuvlee@gmail.com>
2021-08-09 17:46:05 -0400

    App.js only adds provider

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import './App.css';
 import React, { useEffect } from 'react';
 import { BreakpointProvider, setDefaultBreakpoints } from 'react-socks';
-import { Provider } from 'react-redux';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { Provider, connect } from 'react-redux';
+import Amplify from 'aws-amplify';
 import SchedulePage from './pages/SchedulePage';
 import colors from './styles/colors';
 import { settableBreakpoints } from './styles/breakpoints';
-import store from './redux/store';
+import { store } from './redux/store';
 import awsconfig from './aws-exports';
-import { createStation } from './graphql/mutations';
-import { listStations } from './graphql/queries';
 
 Amplify.configure(awsconfig);
-// const todo = { name: 'my fist todo', description: 'hellow owrld' };
-// console.log(API.graphql(graphqlOperation(listTodos)));
 
 function App() {
   setDefaultBreakpoints(settableBreakpoints());
-  useEffect(() => {
-    console.log('in app UE, should happen only once');
-    async function getStations() {
-      if (true) {
-        const response = await API.graphql(graphqlOperation(listStations));
-        console.log('response');
-        console.log(response.data.listStations.items);
-      } else {
-        console.log('trying to post');
-        const station = { name: 'my fist station', abbrev: 'STN1' };
-        console.log(
-          API.graphql(graphqlOperation(createStation, { input: station }))
-        );
-        console.log('////');
-      }
-    }
-    getStations();
-  }, []);
   return (
     <Provider store={store}>
       <BreakpointProvider
