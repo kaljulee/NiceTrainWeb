ff19fd42b14ad1ec793e3d0aa3eadb3251b76e81
Kalju Lee <kaljuvlee@gmail.com>
2021-08-12 11:51:44 -0400

    amplify reset

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Amplify from 'aws-amplify';
+// import Amplify from 'aws-amplify';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import config from './aws-exports';
+// import config from './aws-exports';
 
-Amplify.configure(config);
+// Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
