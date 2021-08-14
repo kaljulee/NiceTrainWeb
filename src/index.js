8b170eafe253063a1e0d6357b0847663b0878340
Kalju Lee <kaljuvlee@gmail.com>
2021-08-14 09:15:04 -0400

    amplify try 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Amplify from 'aws-amplify';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  // eslint-disable-next-line no-undef
   document.getElementById('root')
 );
 
