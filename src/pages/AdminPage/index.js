66ac71b61300339542e0e2defa0bfbd09c17d133
Kalju Lee <kaljuvlee@gmail.com>
2021-08-16 09:35:09 -0400

    react-list as temporary solution, eventually change to react table?

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-// import 'react-tabs/style/react-tabs.css';
+import ReactList from 'react-list';
 import '../../styles/css/custom-react-tabs.css';
 import {
   AmplifySignOut,
@@ -20,6 +20,9 @@ import colors from '../../styles/colors';
 function AdminPage(props) {
   const [authState, setAuthState] = useState();
   const [user, setUser] = useState();
+  const stations = useSelector((state) => state.stations);
+  console.log('stations in admin');
+  console.log(stations);
   useEffect(
     () =>
       onAuthUIStateChange((nextAuthState, authData) => {
@@ -59,7 +62,51 @@ function AdminPage(props) {
               <Tab>Information Message</Tab>
             </TabList>
             <TabPanel style={{ height: '100%' }}>
-              <h2>station panel</h2>
+              <div style={{ display: 'flex' }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    flex: 4,
+                    flexDirection: 'column',
+                    marginRight: '2vw',
+                    marginLeft: '2vw'
+                  }}
+                >
+                  <h2 style={{ color: 'goldenrod' }}>Stations</h2>
+                  <div style={{ maxHeight: '60vh', overflow: 'auto' }}>
+                    <ReactList
+                      type="uniform"
+                      length={stations.length}
+                      itemRenderer={(index, key) => (
+                        <div
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            backgroundColor: 'ghostwhite',
+                            paddingTop: '1vh',
+                            paddingBottom: '1vh'
+                          }}
+                          key={key}
+                        >
+                          <div>{stations[index].name}</div>
+                          <div>{stations[index].abbrev}</div>
+                        </div>
+                      )}
+                    />
+                  </div>
+                </div>
+                <div
+                  style={{
+                    flex: 5,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    marginLeft: '2vw',
+                    marginRight: '2vw'
+                  }}
+                >
+                  <h2 style={{ color: 'goldenrod' }}>current station</h2>
+                </div>
+              </div>
             </TabPanel>
             <TabPanel>
               <h2>yt panel</h2>
