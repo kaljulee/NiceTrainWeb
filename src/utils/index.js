8d7867c9a288f036f6dc18a0eb993005888aed80
Kalju Lee <kaljuvlee@gmail.com>
2021-08-18 17:49:42 -0400

    createOption takes an arg for label field

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,18 @@
-export function createOption(item) {
-  return { label: item.description, value: item.id };
+export function createOption(item, labelField) {
+  return { label: item[labelField], value: item.id };
 }
 
-export function getCurrentOption(items, id) {
+export function getCurrentOption(items, id, labelField) {
   const currentItem = items.find((item) => item.id === id);
   if (!currentItem) {
     return {};
   }
-  return createOption(currentItem);
+  return createOption(currentItem, labelField);
+}
+
+export function formatDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}-${month.length < 2 ? '0' : ''}${month}-${day}`;
 }
