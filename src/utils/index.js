199076597b3e6cc7185902e414bf15e2edce592f
Kalju Lee <kaljuvlee@gmail.com>
2021-08-19 15:39:22 -0400

    seconds and hms conversion functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -16,3 +16,33 @@ export function formatDate(date) {
   const day = date.getDate();
   return `${year}-${month.length < 2 ? '0' : ''}${month}-${day}`;
 }
+
+function pullTime(total, unit) {
+  if (total > unit - 1) {
+    const overflow = total % unit;
+    const trackedTime = total - overflow;
+    return { tracked: trackedTime / unit, untracked: overflow };
+  }
+  return { tracked: 0, untracked: total };
+}
+
+export function HMSToSeconds(hms) {
+  if (!hms || isNaN(hms.h) || isNaN(hms.m) || isNaN(hms.s)) {
+    return NaN;
+  }
+
+  return hms.h * 3600 + hms.m * 60 + hms.s;
+}
+
+export function secondsToHMS(time) {
+  let untrackedTime = time;
+
+  const pulledHours = pullTime(untrackedTime, 3600);
+  const h = pulledHours.tracked;
+  untrackedTime = pulledHours.untracked;
+
+  const pulledMinutes = pullTime(untrackedTime, 60);
+  const m = pulledMinutes.tracked;
+  const s = pulledMinutes.untracked;
+  return { h, m, s };
+}
