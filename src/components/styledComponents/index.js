89a76d8954086a3f3b8d7e440db3e1f37d228493
Kalju Lee <kaljuvlee@gmail.com>
2021-08-19 11:37:08 -0400

    styled basic components

--- src/components/styledComponents/index.js
+++ src/components/styledComponents/index.js
@@ -8,3 +8,24 @@ export const Box = styled.div({
 export const AdminTitle = styled.h3({
   color: 'goldenrod'
 });
+
+export const Row = styled.div({
+  display: 'flex',
+  flexDirection: 'row'
+});
+
+export const Column = styled.div({
+  display: 'flex',
+  flexDirection: 'column'
+});
+
+export const ToggleMenu = styled.div`
+  position: absolute;
+  bottom: 0px;
+  max-height: ${(props) => (props.isOpen ? '100%' : '0')};
+  width: 100%;
+  background-color: pink;
+  background: pink;
+  overflow: hidden;
+  transition: all 0.3s ease-out;
+`;
