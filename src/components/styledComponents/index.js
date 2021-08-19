224a79900dd1b4d00e4c1b8430a39f7fbeac03eb
Kalju Lee <kaljuvlee@gmail.com>
2021-08-19 10:21:00 -0400

    emotion styled basic components file

---
+++ src/components/styledComponents/index.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Box = styled.div({
+  width: '100%',
+  height: '100%'
+});
+
+export const AdminTitle = styled.h3({
+  color: 'goldenrod'
+});
