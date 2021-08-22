efffb4f020b9f02b8130a5010ef3c28bcfbadbbc
Kalju Lee <kaljuvlee@gmail.com>
2021-08-22 14:51:03 -0400

    rename basic components to NTComponents

--- src/pages/AdminPage/Panels/ScheduledActivityPanel/TrainActivitiesEditor.js
+++ src/pages/AdminPage/Panels/ScheduledActivityPanel/TrainActivitiesEditor.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Box, Row } from '../../../../components/layoutComponents';
+import { NTBox, NTRow } from '../../../../components/layoutComponents';
 import ScheduledActivityDnD from './ScheduledActivityDnD';
 
-const ActivityFieldTabs = styled(Box)`
+const ActivityFieldTabs = styled(NTBox)`
   background: green;
 `;
 
 function TrainActivitiesEditor(props) {
   const { scheduledActivities } = props;
   return (
-    <Row style={{ border: '1px solid black', height: '100%' }}>
+    <NTRow style={{ border: '1px solid black', height: '100%' }}>
       <div style={{ flex: 8 }}>
         <ScheduledActivityDnD scheduledActivities={scheduledActivities} />
       </div>
       <div style={{ flex: 3 }}>
         <ActivityFieldTabs />
       </div>
-    </Row>
+    </NTRow>
   );
 }
 
