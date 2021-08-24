458ed9666c242f9c935b51d7bb64674dd2cffa34
Kalju Lee <kaljuvlee@gmail.com>
2021-08-24 08:13:20 -0400

    board train information selector

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,25 +1,30 @@
-import { createDraftSafeSelector } from '@reduxjs/toolkit';
+import { createSelector } from '@reduxjs/toolkit';
 
-// todo this needs to use createSelector
-export const boardTrainInformation = (state) => {
-  const { scheduledTrains, stations } = state.train;
-  return scheduledTrains.map((train) => {
-    const {
-      stationID,
-      groundTag,
-      standingTag,
-      status,
-      train_time,
-      train_date
-    } = train;
-    const station = stations.find((s) => s.id === stationID);
-    return {
-      date: train_date,
-      time: train_time,
-      station: station ? station.abbrev : '',
-      status,
-      groundTag,
-      standingTag
-    };
-  });
-};
+const trainSelector = (state) => state.train.scheduledTrains;
+const stationSelector = (state) => state.train.stations;
+
+export const boardTrainInformation = createSelector(
+  trainSelector,
+  stationSelector,
+  (trains, stations) =>
+    trains.map((train) => {
+      const {
+        stationID,
+        groundTag,
+        standingTag,
+        status,
+        train_time,
+        train_date
+      } = train;
+      const station = stations.find((s) => s.id === stationID);
+      const time = train_time.replace(/[:]/g, '');
+      return {
+        date: train_date,
+        time,
+        station: station ? station.abbrev : '',
+        status,
+        groundTag,
+        standingTag
+      };
+    })
+);
