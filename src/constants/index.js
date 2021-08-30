0b9f63fcb5c4ff5cc3fd2611816991d9041d918a
Kalju Lee <kaljuvlee@gmail.com>
2021-08-30 10:44:53 -0400

    train status enum setup

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,10 @@ export const apiKey = 'API_KEY';
 export const SETTING_TYPE = {
   ACTIVE_LONG_MESSAGE: 'ACTIVE_LONG_MESSAGE'
 };
+
+export const TRAIN_STATUS = {
+  ON_TIME: 'ON_TIME',
+  DELAYED: 'DELAYED',
+  COMPLETE: 'COMPLETE',
+  MAYBE: 'MAYBE'
+};
