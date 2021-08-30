d150673cd80afc24a51eddc478f7aa065a159c63
Kalju Lee <kaljuvlee@gmail.com>
2021-08-30 17:54:41 -0400

    reduce status and info button sizes

--- src/pages/SchedulePage/components/TrainBoard/InfoButton/index.js
+++ src/pages/SchedulePage/components/TrainBoard/InfoButton/index.js
@@ -19,8 +19,8 @@ function InfoButton(props) {
         style={{
           display: 'flex',
           color: theme.accent,
-          width: '3vh',
-          height: '3vh'
+          width: '2vh',
+          height: '2vh'
         }}
       />
     </StyledButton>
