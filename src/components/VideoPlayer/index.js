43bc83d288c1c1cb398ca4f9b6f2f987c5da837d
Kalju Lee <kaljuvlee@gmail.com>
2021-09-25 14:49:22 -0400

    padding is responsive

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import styled from '@emotion/styled';
 
@@ -14,10 +14,54 @@ const StyledPlayer = styled(ReactPlayer)`
 `;
 
 function VideoPlayer(props) {
-  const { url } = props;
+  const { url, size } = props;
+  const [playerDims, setPlayerDims] = useState([1, 1]);
+  const [paddingStyle, setPaddingStyle] = useState({
+    paddingLeft: 1,
+    paddingRight: 1,
+    paddingTop: 1,
+    paddingBottom: 1
+  });
+
+  useEffect(() => {
+    const { width, height } = size;
+    // get normalized values
+    const normalWidth = Math.floor(width / 16);
+    const normalHeight = Math.floor(height / 9);
+    let newHeight = height;
+    let newWidth = width;
+    // possible dimension scenarios
+    // too tall
+    if (normalWidth < normalHeight) {
+      newHeight = Math.floor(normalWidth * 9);
+      setPlayerDims([width, newHeight]);
+      // 16:9
+    } else if (normalHeight === normalWidth) {
+      setPlayerDims([width, height]);
+      // too wide
+    } else {
+      newWidth = Math.floor(normalHeight * 16);
+      setPlayerDims([newWidth, height]);
+    }
+    // calculate padding
+    const widthPadding = Math.floor((width - newWidth) / 2);
+    const heightPadding = Math.floor((height - newHeight) / 2);
+    setPaddingStyle({
+      paddingTop: heightPadding,
+      paddingBottom: heightPadding,
+      paddingLeft: widthPadding,
+      paddingRight: widthPadding
+    });
+  }, [size]);
+
   return (
     <ResponsiveWrapper>
-      <StyledPlayer width="100%" height="100%" url={url} />
+      <StyledPlayer
+        width={`${playerDims[0]}px`}
+        height={`${playerDims[1]}px`}
+        url={url}
+        style={paddingStyle}
+      />
     </ResponsiveWrapper>
   );
 }
