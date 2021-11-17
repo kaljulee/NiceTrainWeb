5829347c05cc0931e896f8b35b87f2035c5c9e15
Kalju <kaljuvle@gmail.com>
2021-11-17 10:45:34 -0500

    add footer to pamphlet admin wrapper

--- src/pages/AdminPage/PamphletAdmin/index.js
+++ src/pages/AdminPage/PamphletAdmin/index.js
@@ -7,12 +7,23 @@ import { mq5 } from '../../../styles/breakpoints';
 const PamphletAdminWrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: grid;
+  grid-template-rows: 95% 5%;
+  grid-template-areas:
+    'pamphlet-admin-body'
+    'pamphlet-admin-footer';
+`;
+
+const PamphletAdminFooter = styled.div`
+  grid-area: pamphlet-admin-footer;
+  background: ${(p) => p.theme.primarySurface};
 `;
 
 function PamphletAdmin() {
   return (
     <PamphletAdminWrapper>
-      <PamphletList />
+      <PamphletList style={{ gridArea: 'pamphlet-admin-body' }} />
+      <PamphletAdminFooter />
     </PamphletAdminWrapper>
   );
 }
